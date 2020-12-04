a5b5e2e5b86ef3839cb4c4e1a09ebd891b62c366
Joseph Karnafel <josephkarnafel@gmail.com>
2020-12-04 09:46:06 -0800

    passing test for ADD_SODA action type

--- src/reducers/soda-list-reducer.js
+++ src/reducers/soda-list-reducer.js
@@ -1,3 +1,16 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, flavor, cans, id } = action;
+  switch (action.type) {
+    case 'ADD_SODA':
+      return Object.assign({}, state, {
+        [id]: {
+          name,
+          flavor,
+          cans,
+          id
+        }
+      });
+    default:
+      return state;
+  }
 };
