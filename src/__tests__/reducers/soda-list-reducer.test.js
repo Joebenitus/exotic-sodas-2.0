6d5fe01353ec6f649edfae7e1ce6f2019b0766a2
Joseph Karnafel <josephkarnafel@gmail.com>
2020-12-04 09:43:39 -0800

    add good fail test for ADD_SODA action

--- src/__tests__/reducers/soda-list-reducer.test.js
+++ src/__tests__/reducers/soda-list-reducer.test.js
@@ -1,7 +1,36 @@
 import sodaListReducer from '../../reducers/soda-list-reducer';
 
 describe('sodaListReducer', () => {
+
+  let action;
+  const sodaData = {
+    name: 'Non-Alcoholic Beer',
+    flavor: 'Bread',
+    cans: '100',
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(sodaListReducer({}, { type: null })).toEqual({});
+  })
+
+  test('Should successfully add new soda data to masterSodaList', () => {
+    const { name, flavor, cans, id } = sodaData;
+    action = {
+      type: 'ADD_SODA',
+      name,
+      flavor,
+      cans,
+      id
+    };
+
+    expect(sodaListReducer({}, action)).toEqual({
+      [id] : {
+        name,
+        flavor,
+        cans,
+        id
+      }
+    })
   })
 })
