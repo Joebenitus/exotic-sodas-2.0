5f3ec238d759c5a34e4df850137cd0ede465fd74
Joseph Karnafel <josephkarnafel@gmail.com>
2020-12-04 16:31:16 -0800

    remove features tap from nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ function Header() {
         <Navbar.Brand href="#home">Joe's Exotic Sodas</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
       </Navbar>
